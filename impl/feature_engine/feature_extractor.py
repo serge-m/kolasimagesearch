fixed overflow processing in words composer, HistogramFeatureExtractor has scaling and uses coarse-grained histograms

impl/feature_engine/feature_extractor.py
@@ -16,9 +16,10 @@ def calculate_histogram_1d(image: np.array) -> Descriptor:
     if not shape_is_good(image):
         raise HistogramBasedFeatureExtractorException("only 1 channel images are supported here")
 
-    image_clipped = np.clip(image, 0, 256)
-    h = np.histogram(a=image_clipped, bins=256, range=[0, 256], density=True)
-    return Descriptor(vector=h[0])
+    image_clipped = np.clip(image, 0, 255)
+    h = np.histogram(a=image_clipped, bins=16, range=[0, 256])[0]
+    histogram_normalized = h / image_clipped.size
+    return Descriptor(vector=histogram_normalized)
 
 
 def get_channels(image) -> List[np.ndarray]:
@@ -43,10 +44,14 @@ class FeatureExtractor:
 
 
 class HistogramFeatureExtractor(FeatureExtractor):
+    _SCALE_FACTOR = 16.
+
     def calculate(self, image: np.ndarray) -> Descriptor:
         channels = get_channels(image)
         descriptors_1d = [calculate_histogram_1d(channel).vector for channel in channels]
-        descriptor = Descriptor(vector=np.hstack(descriptors_1d))
+        vector = np.hstack(descriptors_1d)
+        vector_clipped = np.clip(vector * self._SCALE_FACTOR, 0.0, 1.0)
+        descriptor = Descriptor(vector=vector_clipped)
         return descriptor
 
     def __eq__(self, other):

impl/feature_engine/test/test_feature_extractor.py
@@ -9,6 +9,7 @@ from impl.feature_engine.feature_extractor import HistogramFeatureExtractor, cal
     HistogramBasedFeatureExtractorException
 
 whole_image = np.random.random(size=[10, 20, 3])
+EXPECTED_1D_HISTO_LENGTH = 16
 
 
 class MockingNPFunction:
@@ -23,15 +24,15 @@ class MockingNPFunction:
         raise AssertionError("Argument not matched")
 
 
-class TestCalculateHistogram:
+class TestHistogramFeatureExtractor:
     channel1 = np.arange(12).reshape([3, 4])
     channel2 = np.arange(100, 112).reshape([3, 4])
     channel3 = np.array([100, ] * 6 + [200, ] * 6).reshape([3, 4])
     test_image = np.stack([channel1, channel2, channel3], axis=2)
     test_image_1d = np.stack([channel1], axis=2)
 
-    descr1 = Descriptor([1, 2])
-    descr2 = Descriptor([3, 4])
+    descr1 = Descriptor([0.1 / 16, 1 / 16])
+    descr2 = Descriptor([0.2 / 16, 4])
     descr3 = Descriptor([5, 6])
 
     mocked_calc_histo_1d = MockingNPFunction([channel1, channel2, channel3],
@@ -40,42 +41,42 @@ class TestCalculateHistogram:
     def test_calculate_histogram_shape(self):
         histo = HistogramFeatureExtractor().calculate(self.test_image)
         assert isinstance(histo, Descriptor)
-        assert histo.vector.shape == (256 * 3,)
+        assert histo.vector.shape == (EXPECTED_1D_HISTO_LENGTH * 3,)
 
     def test_for_3_channel(self):
         with mock.patch('impl.feature_engine.feature_extractor.calculate_histogram_1d', new=self.mocked_calc_histo_1d):
             histo = HistogramFeatureExtractor().calculate(self.test_image)
-        assert np.allclose(histo.vector, [1, 2, 3, 4, 5, 6])
+        assert np.allclose(histo.vector, np.array([0.1, 1, 0.2, 1, 1, 1]))
 
     def test_for_1_channel(self):
         with mock.patch('impl.feature_engine.feature_extractor.calculate_histogram_1d', new=self.mocked_calc_histo_1d):
             histo = HistogramFeatureExtractor().calculate(self.test_image_1d)
-        assert np.allclose(histo.vector, [1, 2, 1, 2, 1, 2])
+        assert np.allclose(histo.vector, [0.1, 1] * 3)
 
     def test_for_2d_image(self):
         with mock.patch('impl.feature_engine.feature_extractor.calculate_histogram_1d', new=self.mocked_calc_histo_1d):
             histo = HistogramFeatureExtractor().calculate(self.channel1)
-        assert np.allclose(histo.vector, [1, 2, 1, 2, 1, 2])
+        assert np.allclose(histo.vector, [0.1, 1] * 3)
 
 
 class TestCalculateHistogram1D:
     channel1 = np.arange(12).reshape([3, 4])
-    expected_histo1 = np.zeros(shape=[256])
-    expected_histo1[:12] = 1 / 12
+    expected_histo1 = np.zeros(shape=[EXPECTED_1D_HISTO_LENGTH])
+    expected_histo1[0] = 1
 
     channel2 = np.arange(100, 112).reshape([3, 4])
-    expected_histo2 = np.zeros(shape=[256])
-    expected_histo2[100:112] = 1 / 12
+    expected_histo2 = np.zeros(shape=[EXPECTED_1D_HISTO_LENGTH])
+    expected_histo2[EXPECTED_1D_HISTO_LENGTH * 100 // 256] = 1
 
     channel3 = np.array([100, ] * 6 + [200, ] * 6).reshape([3, 4])
-    expected_histo3 = np.zeros(shape=[256])
-    expected_histo3[100] = 6 / 12
-    expected_histo3[200] = 6 / 12
+    expected_histo3 = np.zeros(shape=[EXPECTED_1D_HISTO_LENGTH])
+    expected_histo3[EXPECTED_1D_HISTO_LENGTH * 100 // 256] = 0.5
+    expected_histo3[EXPECTED_1D_HISTO_LENGTH * 200 // 256] = 0.5
 
-    channel4 = np.array([-100, ] * 6 + [1000, ] * 6).reshape([3, 4])
-    expected_histo4 = np.zeros(shape=[256])
-    expected_histo4[0] = 6 / 12
-    expected_histo4[255] = 6 / 12
+    channel4 = np.array([-100, ] * 4 + [1000, ] * 8).reshape([3, 4])
+    expected_histo4 = np.zeros(shape=[EXPECTED_1D_HISTO_LENGTH])
+    expected_histo4[0] = 1 / 3
+    expected_histo4[-1] = 2 / 3
 
     def test_return_type(self):
         descriptor = calculate_histogram_1d(self.channel1)

impl/storage/search_words/words_composer.py
@@ -6,14 +6,24 @@ class WordsComposerException(Exception):
 
 
 class WordsComposer:
+    _DESTINATION_TYPE = np.int
+    _MAX_VALUE = np.iinfo(_DESTINATION_TYPE).max
+
     def __init__(self, number_of_words, word_length, number_of_levels):
         self._word_length = word_length
         self._number_of_words = number_of_words
         self._number_of_levels = number_of_levels
-        self._vector_multiplier = number_of_levels ** np.arange(word_length)
+        self._vector_multiplier = self._build_multiplier(number_of_levels, word_length)
 
     def compose(self, quantized_array: np.ndarray) -> np.ndarray:
         return np.dot(self._matrix_of_data(quantized_array), self._vector_multiplier)
+
+    def _build_multiplier(self, number_of_levels, word_length):
+        multiplier = number_of_levels ** np.arange(word_length, dtype=self._DESTINATION_TYPE)
+        if self._has_too_big_values(multiplier):
+            raise WordsComposerException("Configuration of words composer yields integer overflow")
+
+        return multiplier
 
     def _matrix_of_data(self, quantized_array):
         try:
@@ -22,4 +32,6 @@ class WordsComposer:
             raise WordsComposerException("Failed compose search terms. "
                                          "Wrong descriptor shape. '{}'".format(quantized_array.shape)) from e
 
+    def _has_too_big_values(self, multiplier):
+        return multiplier.max() > self._MAX_VALUE / self._number_of_levels / self._number_of_levels
 