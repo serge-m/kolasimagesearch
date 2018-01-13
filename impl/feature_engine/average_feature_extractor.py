eca79444fe19f0e7ba3f06c0fb81e89407ff66c5
Sergey Matuynin <sbmatyunin@gmail.com>
2018-01-13 22:37:55 +0100

    added a draft for average feature extractor

---
+++ impl/feature_engine/average_feature_extractor.py
@@ -0,0 +1,23 @@
+from typing import Iterable
+
+import numpy as np
+
+from impl.feature_engine.histogram_feature_extractor import get_channels
+from kolasimagecommon import Descriptor
+from kolasimagecommon import FeatureExtractor
+
+
+class AverageFeatureExtractor(FeatureExtractor):
+    def calculate(self, image: np.ndarray) -> Descriptor:
+        channels = get_channels(image)
+        average_per_channel = [np.mean(channel)/255. for channel in channels]
+        descriptor = Descriptor(vector=np.array(average_per_channel))
+        return descriptor
+
+    def descriptor_shape(self) -> Iterable[int]:
+        return 3,
+
+    def __eq__(self, other):
+        if isinstance(other, AverageFeatureExtractor):
+            return True
+        return False
